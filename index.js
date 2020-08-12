94bab9cd555c3ba1e9c672c7be34913f9867bc1e
Rick Belson <belsonrj@aol.com>
2020-08-12 23:19:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-function createEmployeeRecord
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
